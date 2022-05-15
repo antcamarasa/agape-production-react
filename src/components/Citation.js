f5d055cff90dc301a8791829fc00bafee224e814
Antoine <camarasa.ltd@gmail.com>
2022-05-15 14:38:20 +0200

    Resize h1 video small device

---
+++ src/components/Citation.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './CitationStyles.css'
+
+const citation = () => {
+  return (
+    <div className='ciation'>
+      <div className='container'>
+        <h3>Le sombre univers, froid, glacé, pesant, réclame</h3>
+        <h3>La sublimation de l’être par la flamme</h3>
+        <h3>De l’homme par l’amour</h3>
+      </div>
+    </div>
+  )
+}
+
+export default citation
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Video from '../components/Video'
+// import Citation from '../components/Citation'
 
 const Home = () => {
     return (
